4a882a6d95adbbf99a320cd23c5627ac7c5de251
Ce Zheng <ce.zheng@gree.net>
2016-09-26 09:33:33 +0900

    Swift 2.3 for Carthage

---
+++ Commander/Commander.h
@@ -0,0 +1,19 @@
+//
+//  Commander.h
+//  Commander
+//
+//  Created by Ce Zheng on 9/23/16.
+//  Copyright © 2016 Ce Zheng. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Commander.
+FOUNDATION_EXPORT double CommanderVersionNumber;
+
+//! Project version string for Commander.
+FOUNDATION_EXPORT const unsigned char CommanderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Commander/PublicHeader.h>
+
+
