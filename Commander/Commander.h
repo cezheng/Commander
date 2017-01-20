a9f53657f637f9c01bfca5bfa92d2f73c09c7206
Ce Zheng <ce.zheng@gree.net>
2017-01-20 19:25:47 +0900

    Support Carthage

---
+++ Commander/Commander.h
@@ -0,0 +1,19 @@
+//
+//  Commander.h
+//  Commander
+//
+//  Created by Ce Zheng on 1/20/17.
+//  Copyright © 2017 kylef. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Commander.
+FOUNDATION_EXPORT double CommanderVersionNumber;
+
+//! Project version string for Commander.
+FOUNDATION_EXPORT const unsigned char CommanderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Commander/PublicHeader.h>
+
+
